95ae11fb7a1398e1900f50533cb3315703ef8008
Philipp Fehr <calvin.philipp@gmail.com>
2017-02-24 08:34:52 +0100

    Updated base content

--- orsum/ajaxResponder.pl
+++ orsum/ajaxResponder.pl
@@ -4,12 +4,14 @@ use warnings;
 use DBI;
 use diagnostics;
 use CGI;
+use JSON;
+use Encode qw(decode_utf8 encode_utf8);
 
-print "Content-Type: text/html\n\n";
+print "Content-Type: application/json; charset=utf-8\n\n";
 
 sub selectEvents {
 	my $db = shift;
-	my $stmt = $db->prepare('SELECT * FROM events');
+	my $stmt = $db->prepare("SELECT events.ID as 'id', events.content as 'content', events.title as 'title', ort.name as 'ort', ort.breiten as 'breiten', ort.laengen as 'laengen', bewegung.ID as 'bewegung', events.moveIndex as 'moveIndex', monat.monat as 'monat', (SELECT jahre.jahr FROM jahre WHERE jahre.ID=monat.jahrIDFS) as 'jahr' FROM `events` JOIN ort ON ort.ID = events.ortIDFS JOIN bewegung ON bewegung.ID = events.bewegungIDFS JOIN monat ON monat.ID = events.monatIDFS");
 	$stmt->execute() or die $stmt->err_str;
 	
 	return $stmt;
@@ -17,20 +19,29 @@ sub selectEvents {
 
 
 my ($db_user, $db_name, $db_pass) = ('3i', 'orsumIchnographiae', 'wurst');
-my $db = DBI->connect("DBI:mysql:database=$db_name", $db_user, $db_pass);
+my $db = DBI->connect("DBI:mysql:database=$db_name", $db_user, $db_pass, {RaiseError => 0, PrintError => 0, mysql_enable_utf8 => 1});
 
 $statement = selectEvents($db);
 
-print "<html><head></head><body>";
-while (my ($col_1, $col_2, $col_3, $col_4) = $statement->fetchrow_array() ) {
-    print "<ul><li>Spalte 1: $col_1 \n</li>";
-    print "<li>Spalte 2: $col_2 \n</li>";
-    print "<li>Spalte 3: $col_3 \n</li>";
-    print "<li>Spalte 4: $col_4 \n</li></ul>";
+my %databaseData;
+while (my ($id, $content, $title, $ort, $breiten, $laengen, $bewegung, $moveIndex, $monat, $jahr) = $statement->fetchrow_array() ) {
+	my %event = (
+		"id" => $id,
+		"content" => encode_utf8($content),
+		"title" => encode_utf8($title),
+		"ort" => encode_utf8($ort),
+		"position" => [$breiten, $laengen],
+		"monat" => $monat,
+		"jahr" => $jahr,
+		"moveIndex" => $moveIndex
+	);
+	
+	if (exists $databaseData{$bewegung}) {
+		push($databaseData{$bewegung}, \%event);
+	} else {
+		$databaseData{$bewegung} = [\%event];
+	}
 }
 
-my $cgi = CGI->new();
-my $param = $q->param('buh');
-print($param);
-
-print "</body></html>";
+my $json = new JSON;
+print $json->encode(\%databaseData);
